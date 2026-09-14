0eef29b384c0eebe5f5eb38636e55f3283bde5c7
Brian Trapp <brian.trapp@globalfoundries.com>
2026-09-14 08:55:33 -0400

    Update pom

--- src/main/java/com/github/btrapp/fastflashfinder/FastFlashFinder.java
+++ src/main/java/com/github/btrapp/fastflashfinder/FastFlashFinder.java
@@ -7,7 +7,6 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -18,56 +17,70 @@ import com.github.btrapp.fastflashfinder.FastFlashObjects.FlashInst;
 import com.github.btrapp.fastflashfinder.FastFlashObjects.FlastFlashExceptionErrorCode;
 
 public class FastFlashFinder {
-	private final TreeMap<Double,ScanEvent> xMap;
-	private final TreeMap<Double,ScanEvent> yMap;
-	private final FlashInst zeroZeroFlash; //The reference (0,0) flash.
-	
-	public  FastFlashFinder (FlashInst zeroZeroFlash, List<FlashDieInst> dies) {
+	private final TreeMap<Double, ScanEvent> xMap;
+	private final TreeMap<Double, ScanEvent> yMap;
+	private final FlashInst zeroZeroFlash; // The reference (0,0) flash.
+
+	/**
+	 * 
+	 * @param zeroZeroFlash the information from the 0,0 flash
+	 * @param dies          a list of all die instances on a flash
+	 */
+	public FastFlashFinder(FlashInst zeroZeroFlash, List<FlashDieInst> dies) {
 		this.zeroZeroFlash = zeroZeroFlash;
 		this.xMap = buildScanMap(dies, FlashDieInst::llx, FlashDieInst::urx);
 		this.yMap = buildScanMap(dies, FlashDieInst::lly, FlashDieInst::ury);
 	}
-	
+
 	public FlashId findFlash(double waferX, double waferY) {
-		//Figure out the difference in X from the zeroZero flash LLx
+		// Figure out the difference in X from the zeroZero flash LLx
 		int dxWaferStepInt = flashStep(waferX, zeroZeroFlash.llx(), zeroZeroFlash.steppingWidth());
-		int dyWaferStepInt = flashStep(waferY, zeroZeroFlash.lly(),zeroZeroFlash.steppingHeight());
-		return new FlashId(dxWaferStepInt,dyWaferStepInt);
+		int dyWaferStepInt = flashStep(waferY, zeroZeroFlash.lly(), zeroZeroFlash.steppingHeight());
+		return new FlashId(dxWaferStepInt, dyWaferStepInt);
 	}
-	
+
+	/**
+	 * Given a wafer level dimension (um), figure out which flash ID would contain
+	 * that dimension. (Notch down, x++ is right, y++ is up)
+	 * 
+	 * @param waferDim
+	 * @param flashStart
+	 * @param flashStep
+	 * @return
+	 */
 	private int flashStep(double waferDim, double flashStart, double flashStep) {
-		double dWafer = waferDim-flashStart;
+		double dWafer = waferDim - flashStart;
 		double step = dWafer / flashStep;
 		return (int) Math.floor(step);
 	}
-	
+
 	/**
-	 * This expects WAFER level coordiantes (notch down, 0,0 at center of wafer)
-	 * @param flashId (@see findFlash(waferX,waferY) to determine this)
+	 * This expects WAFER level coordiantes (notch down, 0,0 at center of wafer, x++
+	 * is right, y++ is up)
+	 * 
+	 * @param flashId (@See findFlash(waferX,waferY) to determine this)
 	 * @param waferX
 	 * @param waferY
 	 * @return
 	 * @throws FastFlashException
 	 */
 	public FlashDieInst findDieInstanceOrNull(FlashId flashId, double waferX, double waferY) throws FastFlashException {
-		//Convert wafer XY into flash-Relative XY
+		// Convert wafer XY into flash-Relative XY
 		double[] dieXY = calculateFlashRelativeXY(flashId, waferX, waferY);
 		return findDieInstanceOrNull(dieXY[0], dieXY[1]);
 	}
-	
-	
-	
+
 	private double[] calculateFlashRelativeXY(FlashId flashIdXy, double waferX, double waferY) {
-		double fx = flashIdXy.flashIdX()*zeroZeroFlash.steppingWidth();
-		double fy = flashIdXy.flashIdY()*zeroZeroFlash.steppingHeight();
-		double dx = waferX-fx;
-		double dy = waferY-fy;
-		return new double[] {dx,dy};
+		double fx = flashIdXy.flashIdX() * zeroZeroFlash.steppingWidth();
+		double fy = flashIdXy.flashIdY() * zeroZeroFlash.steppingHeight();
+		double dx = waferX - fx;
+		double dy = waferY - fy;
+		return new double[] { dx, dy };
 	}
-	
-	//This expects FLASH coordinates (from the LLxy of the correct flash)
+
+	// This expects FLASH coordinates (from the LLxy of the correct flash)
 	protected FlashDieInst findDieInstanceOrNull(double flashX, double flashY) throws FastFlashException {
-		
+
 		Entry<Double, ScanEvent> seX = xMap.floorEntry(flashX);
 		Entry<Double, ScanEvent> seY = yMap.floorEntry(flashY);
 		if (seX == null || seY == null) {
@@ -76,90 +89,102 @@ public class FastFlashFinder {
 		Set<FlashDieInst> xMatches = seX.getValue().matchDieIds(flashX);
 		if (xMatches.isEmpty())
 			return null;
-		
+
 		Set<FlashDieInst> yMatches = seY.getValue().matchDieIds(flashY);
 		yMatches.retainAll(xMatches);
 		if (yMatches.isEmpty())
 			return null;
-		
+
 		if (yMatches.size() == 1)
 			return yMatches.iterator().next();
-		throw new FastFlashObjects.FastFlashException(FlastFlashExceptionErrorCode.OVERLAPPING_DIES, "Overlapping Dies found at FlashXY: "+flashX+","+flashY);
+		throw new FastFlashObjects.FastFlashException(FlastFlashExceptionErrorCode.OVERLAPPING_DIES,
+				"Overlapping Dies found at FlashXY: " + flashX + "," + flashY);
 	}
-	
+
 	private static final class ScanEvent {
 		private double eventKey;
 		private Set<FlashDieInst> endEvents = Set.of();
 		private Set<FlashDieInst> continueEvents = Set.of();
 		private Set<FlashDieInst> startEvents = Set.of();
+
 		public ScanEvent(double key) {
-			this.eventKey=key;
+			this.eventKey = key;
 		}
+
 		public void setEndEvents(Set<FlashDieInst> endEvents) {
 			this.endEvents = endEvents;
 		}
+
 		public void setContinueEvents(Set<FlashDieInst> continueEvents) {
 			this.continueEvents = continueEvents;
 		}
+
 		public void setStartEvents(Set<FlashDieInst> startEvents) {
 			this.startEvents = startEvents;
 		}
+
 		@Override
 		public String toString() {
-			//A debug string
-			Function<Set<FlashDieInst>,String> toStr = (s)-> s.stream().map(i->i.toString()).collect(Collectors.joining(","));
-			String s = "Scan Event for key "+eventKey+"\n";
-			s += "Strt: "+toStr.apply(startEvents)+"\n";
-			s += "Cont: "+toStr.apply(continueEvents)+"\n";
-			s += "Ends: "+toStr.apply(endEvents)+"\n";
+			// A debug string
+			Function<Set<FlashDieInst>, String> toStr = (s) -> s.stream().map(i -> i.toString())
+					.collect(Collectors.joining(","));
+			String s = "Scan Event for key " + eventKey + "\n";
+			s += "Strt: " + toStr.apply(startEvents) + "\n";
+			s += "Cont: " + toStr.apply(continueEvents) + "\n";
+			s += "Ends: " + toStr.apply(endEvents) + "\n";
 			return s;
 		}
+
 		public Set<FlashDieInst> matchDieIds(double key) {
 			Set<FlashDieInst> matches = new HashSet<>();
 			matches.addAll(continueEvents);
-			if (key == eventKey) 
-				matches.addAll(endEvents); //If we end exactly on this key, its included.
+			if (key == eventKey)
+				matches.addAll(endEvents); // If we end exactly on this key, its included.
 			if (key > eventKey)
-				matches.addAll(startEvents); //only include start events if the key is **after** (not equal to) the event key!
-			 
+				matches.addAll(startEvents); // only include start events if the key is **after** (not equal to) the
+												// event key!
+
 			return matches;
 		}
 	}
 
-	private static TreeMap<Double,ScanEvent> buildScanMap(List<FlashDieInst> dies, Function<FlashDieInst,Double> startKey,  Function<FlashDieInst,Double> endKey) {
-		Map<Double,Set<FlashDieInst>> startEvents = dies.stream().collect(Collectors.groupingBy(startKey,Collectors.mapping(Function.identity(),Collectors.toSet())));
-		Map<Double,Set<FlashDieInst>> endEvents = dies.stream().collect(Collectors.groupingBy(endKey,Collectors.mapping(Function.identity(),Collectors.toSet())));
-		
+	private static TreeMap<Double, ScanEvent> buildScanMap(List<FlashDieInst> dies,
+			Function<FlashDieInst, Double> startKey, Function<FlashDieInst, Double> endKey) {
+		Map<Double, Set<FlashDieInst>> startEvents = dies.stream()
+				.collect(Collectors.groupingBy(startKey, Collectors.mapping(Function.identity(), Collectors.toSet())));
+		Map<Double, Set<FlashDieInst>> endEvents = dies.stream()
+				.collect(Collectors.groupingBy(endKey, Collectors.mapping(Function.identity(), Collectors.toSet())));
+
 		Set<Double> sortedKeys = new TreeSet<>(startEvents.keySet());
 		sortedKeys.addAll(endEvents.keySet());
-		
-		Set<FlashDieInst> activeDies = new HashSet<>(); //Tracks dies that have started but not ended yet
-		
-		TreeMap<Double,ScanEvent> scanEvents = new TreeMap<>();
-		for (Double key: sortedKeys) {
+
+		Set<FlashDieInst> activeDies = new HashSet<>(); // Tracks dies that have started but not ended yet
+
+		TreeMap<Double, ScanEvent> scanEvents = new TreeMap<>();
+		for (Double key : sortedKeys) {
 			ScanEvent se = new ScanEvent(key);
-			
-			//Process END events
+
+			// Process END events
 			Set<FlashDieInst> endEventList = endEvents.get(key);
 			if (endEventList != null) {
 				se.setEndEvents(endEventList);
-				//Remove these IDs from the active list
+				// Remove these IDs from the active list
 				activeDies.removeAll(endEventList);
 			}
-			
-			//Process 'Continue' events
+
+			// Process 'Continue' events
 			if (!activeDies.isEmpty()) {
-				se.setContinueEvents(new HashSet<>(activeDies)); //Use a new set as we will modify activeDies object
+				se.setContinueEvents(new HashSet<>(activeDies)); // Use a new set as we will modify activeDies object
 			}
-			
-			//Process 'Start' events
+
+			// Process 'Start' events
 			Set<FlashDieInst> startEventList = startEvents.get(key);
 			if (startEventList != null) {
 				se.setStartEvents(startEventList);
-				//Add these IDs from the active list for the *next* key
+				// Add these IDs from the active list for the *next* key
 				activeDies.addAll(startEventList);
 			}
-			
+
 			scanEvents.put(key, se);
 		}
 		return scanEvents;
